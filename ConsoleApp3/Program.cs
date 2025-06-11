b89a8fc1ae3a29c5ddd79e78f0eaa99d6929f970
Miguel Alfonso Gutib <vsparda036@gmail.com>
2025-06-11 10:11:30 +0800

    ACTIVITY 4

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,13 +2,13 @@
 {
     static void Main(string[] args)
     {
-        Time time1 = new Time(10, 5);   // 10:05
-        Time time2 = new Time(0, 45);   // 00:45
-        Time time3 = new Time(23, 59);  // 23:59
+        Time time1 = new Time(10, 5);   
+        Time time2 = new Time(0, 45);   
+        Time time3 = new Time(1, 15);
 
-        Console.WriteLine("Time 1 (minutes since midnight): " + time1);
-        Console.WriteLine("Time 2 (minutes since midnight): " + time2);
-        Console.WriteLine("Time 3 (minutes since midnight): " + time3);
+        Console.WriteLine("Time1: " + time1);
+        Console.WriteLine("Time2: " + time2);
+        Console.WriteLine("Time2: " + time3);
     }
 }
 
--- ConsoleApp3/Timestruct.cs
+++ ConsoleApp3/Timestruct.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-public struct Time
+﻿public struct Time
 {
     private readonly int minutes;
 
@@ -9,8 +7,18 @@ public struct Time
         this.minutes = 60 * hh + mm;
     }
 
+    public int Hour
+    {
+        get { return minutes / 60; }
+    }
+
+    public int Minute
+    {
+        get { return minutes % 60; }
+    }
+
     public override string ToString()
     {
-        return minutes.ToString();
+        return string.Format("{0:D2}:{1:D2}", Hour, Minute);
     }
 }
