91109ccffdbffa98f88b0677a127b2d02b3fd16d
Miguel Alfonso Gutib <vsparda036@gmail.com>
2025-06-11 15:29:47 +0800

    Activity 5

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,13 +2,24 @@
 {
     static void Main(string[] args)
     {
-        Time time1 = new Time(10, 5);   
-        Time time2 = new Time(0, 45);   
-        Time time3 = new Time(1, 15);
+        Time t1 = new Time(1, 30); // 01:30
+        Time t2 = new Time(0, 45); // 00:45
+        Time t3 = new Time(1, 15);
 
-        Console.WriteLine("Time1: " + time1);
-        Console.WriteLine("Time2: " + time2);
-        Console.WriteLine("Time2: " + time3);
+        
+
+
+        Time sum = t1 + t2;
+        Time diff = t1 - t2;
+
+        Time reverseDiff = t2 - t1;
+
+        Console.WriteLine("t1: " + t1);      
+        Console.WriteLine("t2: " + t2);
+        Console.WriteLine("t3: " + t3 + Environment.NewLine); 
+
+        Console.WriteLine("t1 + t2 = " + sum);     
+        Console.WriteLine("t1 - t2 = " + diff);    
+        Console.WriteLine("t2 - t1 = " + reverseDiff); 
     }
 }
-
--- ConsoleApp3/Timestruct.cs
+++ ConsoleApp3/Timestruct.cs
@@ -7,18 +7,29 @@
         this.minutes = 60 * hh + mm;
     }
 
-    public int Hour
-    {
-        get { return minutes / 60; }
-    }
+    public int Hour => minutes / 60;
 
-    public int Minute
-    {
-        get { return minutes % 60; }
-    }
+    public int Minute => minutes % 60;
 
     public override string ToString()
     {
         return string.Format("{0:D2}:{1:D2}", Hour, Minute);
+    }
+
+    // Overload + operator
+    public static Time operator +(Time t1, Time t2)
+    {
+        return new Time(0, t1.minutes + t2.minutes);
+    }
+
+    // Overload - operator
+    public static Time operator -(Time t1, Time t2)
+    {
+        int diff = t1.minutes - t2.minutes;
+        if (diff < 0)
+        {
+            diff = 0; 
+        }
+        return new Time(0, diff);
     }
 }
