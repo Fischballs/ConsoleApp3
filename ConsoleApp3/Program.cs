b397030a52f3c21f45b90453c4708d71cf1fda12
Miguel Alfonso Gutib <vsparda036@gmail.com>
2025-06-11 09:57:18 +0800

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Time time1 = new Time(10, 5);   // 10:05
+        Time time2 = new Time(0, 45);   // 00:45
+        Time time3 = new Time(23, 59);  // 23:59
+
+        Console.WriteLine("Time 1 (minutes since midnight): " + time1);
+        Console.WriteLine("Time 2 (minutes since midnight): " + time2);
+        Console.WriteLine("Time 3 (minutes since midnight): " + time3);
+    }
+}
+
---
+++ ConsoleApp3/ProgramHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public struct Time
+{
+    private readonly int minutes;
+
+    public Time(int hh, int mm)
+    {
+        this.minutes = 60 * hh + mm;
+    }
+
+    public override string ToString()
+    {
+        return minutes.ToString();
+    }
+}
